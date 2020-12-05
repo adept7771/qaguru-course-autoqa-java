46ea323ee6f41789eccb659474faddb1405b7f14
dmitrijpotapov <dmitry.potapov@btsdigital.kz>
2020-12-05 17:52:33 +0300

    test already complete

--- src/test/java/project/YandexTest.java
+++ src/test/java/project/YandexTest.java
@@ -6,28 +6,16 @@ import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YandexTest {
-  String keyWord = "selenide";
-
   @Test
-  void yandexTest()  {
-    open("https://yandex.ru");
-    $x("//input[@aria-label='Запрос']").setValue("Selenide").pressEnter();
-    int assertionsMatches = 0;
-    for(SelenideElement element : $$x("//div[@class='organic__url-text']")){
-      if(element.text().toLowerCase().contains(keyWord)){
-        assertionsMatches += 1;
-      }
-    }
-    Assertions.assertTrue(assertionsMatches >= 5,
-            "Matches for search is: " + assertionsMatches + " it's less then expected (5)");
+  void successTest() {
+    assertEquals(2 * 2, 4);
   }
 
   @Test
-  void searchGoogle() {
-    open("https://google.com/");
-    $(byName("q")).val(keyWord).pressEnter();
-    $("#search").shouldHave(Condition.text("selenide.org"));
+  void negativeTest() {
+    assertEquals(2 * 2, 5);
   }
 }
