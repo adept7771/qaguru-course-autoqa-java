7321c221c1a30e338d757e78e255a1e27f4ae966
dmitrijpotapov <dmitry.potapov@btsdigital.kz>
2020-12-06 20:03:42 +0300

    develop test

--- src/test/java/project/SelenideTest.java
+++ src/test/java/project/SelenideTest.java
@@ -6,29 +6,42 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.*;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
+  String firstName = "firstName";
+  String lastName = "lastName";
+  String email = "test@email.kz";
+  String mobileNumber = "1234567890";
+  String address = "Улица Пушкина, дом Колотушкина";
+  String subject = "English";
+
+
   @Test
-  void alfabankOldDepositsTest()  {
-    open("https://alfabank.ru/make-money/");
-    $x("//button[text()='Архивные счета и депозиты']").click();
-    $$x("//div[@data-widget-name='CatalogCard']")
-            .shouldHave(CollectionCondition.size(5));
+  void demoQaTest() throws InterruptedException {
+    open("https://demoqa.com/automation-practice-form");
+    $x("//input[@id='firstName']").setValue(firstName);
+    $x("//input[@id='lastName']").setValue(lastName);
+    $x("//input[@id='userEmail']").setValue(email);
+    $x("//label[@for='gender-radio-1']").click();
+    $x("//input[@id='userNumber']").setValue(mobileNumber);
+    $x("//input[@id='dateOfBirthInput']").click();
+    $x("//select[@class='react-datepicker__month-select']").selectOptionContainingText("July");
+    $x("//select[@class='react-datepicker__year-select']").selectOptionContainingText("2010");
+    $x("//div[contains(@class, 'react-datepicker__day--014')]").click();
+    //$x("//input[@id='subjectsInput']").setValue(subject);
+    $("#subjectsInput").setValue(subject);
+    $x("//div[text()='English']").click();
+    $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/test.png"));
+    $x("//textarea[@id='currentAddress']").setValue(address);
+    $x("//div[text()='NCR']").selectOptionContainingText("Haryana");
+    $x("//div[text()='Select City']").selectOptionContainingText("Panipat");
+    $x("//button[@id='submit']").click();
+    //Thread.sleep(20000);
+
   }
-
-  @Test // страхования не нашел. Выбрал другой пункт выпадающего меню "Альфа счет"
-  void siblingPrecedingClosestParentTest()  {
-    open("https://alfabank.ru/make-money/");
-    $x("//a[@title='Вклады']").hover();
-    $x("//div[@data-test-id='Main-Header-Main-DesktopLayout']")
-            .closest("href=\"/make-money/savings-account/alfa/\"").click();
-    $x("//h1[text()='Накопительный Альфа-Счёт']").shouldBe(Condition.appear);
-    Selenide.back();
-
-  }
-
-
 }
