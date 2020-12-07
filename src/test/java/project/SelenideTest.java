62a3f394266540380035cbeace7c03cc981cdfc3
dmitrijpotapov <dmitry.potapov@btsdigital.kz>
2020-12-07 10:21:06 +0300

    develop test

--- src/test/java/project/SelenideTest.java
+++ src/test/java/project/SelenideTest.java
@@ -1,9 +1,13 @@
 package project;
 
 import com.codeborne.selenide.*;
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
@@ -55,6 +59,9 @@ public class SelenideTest {
     $(byText(state)).click();
     $("#city").click();
     $(byText(city)).click();
+
+    $("#subjectsInput").val("e");
+
     $x("//button[@id='submit']").click();
 
     $x("//td[text()='Student Name']/following-sibling::td[1]")
@@ -76,4 +83,54 @@ public class SelenideTest {
     $x("//td[text()='State and City']/following-sibling::td[1]")
                 .shouldHave(text(state + " " + city));
   }
+
+  @Test
+  void successfulRegistrationTest() {
+    Faker faker = new Faker();
+    FakeValuesService fakeValuesService = new FakeValuesService(
+            new Locale("en-GB"), new RandomService());
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String userEmail = fakeValuesService.bothify("????##@gmail.com");
+    String userNumber = fakeValuesService.regexify("[0-9]{10}");
+    String address = faker.address().fullAddress();
+    String subject = "Biology";
+    String gender = "Male";
+    String hobbies = "Sports";
+    String date = "03 December,2020";
+    String state = "NCR";
+    String city = "Delhi";
+
+    open("https://demoqa.com/automation-practice-form");
+    $("#firstName").val(firstName);
+    $("#lastName").val(lastName);
+    $("#userEmail").val(userEmail);
+    $x("//*[@for='gender-radio-1']").click();
+    $("#userNumber").val(userNumber);
+    $("#dateOfBirthInput").click();
+    $(".react-datepicker__day--003").click();
+    $("#subjectsInput").click();
+    $("#subjectsInput").val(subject);
+    $$("div[id^=\"react-select-2-option\"]").find(text(subject)).click();
+    $x("//*[@for='hobbies-checkbox-1']").click();
+
+    $("#currentAddress").val(address);
+    $("#state").scrollTo().click();
+    $(byText(state)).click();
+    $("#city").click();
+    $(byText(city)).click();
+    $("#submit").scrollTo().click();
+
+    $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    $x("*//tr[1]/td[2]").shouldHave(text(firstName + " " + lastName));
+    $x("*//tr[2]/td[2]").shouldHave(text(userEmail));
+    $x("*//tr[3]/td[2]").shouldHave(text(gender));
+    $x("*//tr[4]/td[2]").shouldHave(text(userNumber));
+    $x("*//tr[5]/td[2]").shouldHave(text(date));
+    $x("*//tr[6]/td[2]").shouldHave(text(subject));
+    $x("*//tr[7]/td[2]").shouldHave(text(hobbies));
+    $x("*//tr[9]/td[2]").shouldHave(text(address));
+    $x("*//tr[10]/td[2]").shouldHave(text(state + " " + city));
+  }
+
 }
