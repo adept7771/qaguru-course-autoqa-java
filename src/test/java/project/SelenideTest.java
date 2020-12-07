dbeffcc6aee18f55d20b8c0858cb94b16ea28e76
dmitrijpotapov <dmitry.potapov@btsdigital.kz>
2020-12-07 14:54:54 +0300

    develop test

--- src/test/java/project/SelenideTest.java
+++ src/test/java/project/SelenideTest.java
@@ -2,33 +2,37 @@ package project;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.*;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
-  @Test
-  void alfabankOldDepositsTest()  {
-    open("https://alfabank.ru/make-money/");
-    $x("//button[text()='Архивные счета и депозиты']").click();
-    $$x("//div[@data-widget-name='CatalogCard']")
-            .shouldHave(CollectionCondition.size(5));
-  }
+    @Test
+    void alfabankOldDepositsTest() {
+        open("https://alfabank.ru/make-money/");
+        $x("//button[text()='Архивные счета и депозиты']").click();
+        $$x("//div[@data-widget-name='CatalogCard']")
+                .shouldHave(CollectionCondition.size(5));
+    }
 
-  @Test // страхования не нашел. Выбрал другой пункт выпадающего меню "Альфа счет"
-  void siblingPrecedingClosestParentTest()  {
-    open("https://alfabank.ru/make-money/");
-    $x("//a[@title='Вклады']").hover();
-    $x("//div[@data-test-id='Main-Header-Main-DesktopLayout']")
-            .closest("href=\"/make-money/savings-account/alfa/\"").click();
-    $x("//h1[text()='Накопительный Альфа-Счёт']").shouldBe(Condition.appear);
-    Selenide.back();
-
-  }
-
-
+    @Test
+        // страхования не нашел. Выбрал другой пункт выпадающего меню "Накопительные счета"
+    void siblingPrecedingClosestParentTest() {
+        open("https://alfabank.ru/make-money/");
+        $x("//a[@title='Вклады']").hover();
+        $x("//div[text()='Накопительные счета']").click();
+        // вариации на тему наследников:
+        $x("//*[contains(text(),'Страхование вкладов')]").closest("button").click();
+        $x("//span[text()='Описание']").parent().click();
+        $(Selectors.withText("Открыть накопительный счёт"))
+                .should(Condition.appear);
+        $x("//button[@data-test-id='tabs-list-tabTitle-0']").sibling(1).click();
+        $(Selectors.withText("Что такое вклад"))
+                .should(Condition.appear);
+        $x("//button[@data-test-id='tabs-list-tabTitle-2']").preceding(0).click();
+        $(Selectors.withText("Федеральный закон от 23.12.2003"))
+                .should(Condition.appear);
+    }
 }
